03772b8c355dc82479a520e6cdc2140c31a1570a
zubinengg <zubin.engg@gmail.com>
2015-07-17 21:38:21 +0530

    Success_1

--- public_html/index.js
+++ public_html/index.js
@@ -1,32 +1,39 @@
 "use strict";
 
+var canvas;
 var gl;
+
 var points = [];
-var nsd = 5;
-var theta = 90;
-var thetaLoc;
-var origin = vec2(0, 0);
-window.onload = function init() {
-    var canvas = document.getElementById("gl-canvas");
+
+var numTimesToSubdivide = 0;
+
+var bufferId;
+
+var theta = 0;
+
+var sides = 3;
+
+function init()
+{
+    canvas = document.getElementById("gl-canvas");
 
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) {
         alert("WebGL isn't available");
     }
 
-    //var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
-    var vertices = [vec2(-1/2, -1/2), vec2(0, 1/2), vec2(1/2, -1/2)];
-    divideTriangle(vertices[0], vertices[1], vertices[2], nsd);
-    transform();
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
 
+    // First, initialize the corners of our gasket with three points.
+
+
+    //
     //  Configure WebGL
-    console.log(points.length);
-    console.log("testing log");
-    console.log(vertices.length);
-    
-
+    //
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1, 1, 0.5, 1.0);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     //  Load shaders and initialize attribute buffers
 
@@ -35,28 +42,66 @@ window.onload = function init() {
 
     // Load the data into the GPU
 
-    var bufferId = gl.createBuffer();
+    bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, 8 * Math.pow(3, 6), gl.STATIC_DRAW);
+
+
 
     // Associate out shader variables with our data buffer
 
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-    
-    document.getElementById("slider").onchange = function () {
-       thetaLoc = 360 - event.srcElement.value;
+
+    document.getElementById("slider").onchange = function (target) {
+        numTimesToSubdivide = parseInt(event.target.value);
+        render();
     };
-    //transform();
-    console.log("Theta");
-    console.log(theta);
-    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    document.getElementById("slider_a").onchange = function (target) {
+        theta = parseInt(event.target.value);
+        render();
+    };
+
 
     render();
-};
+}
+;
+
+function triangle(a, b, c)
+{
+    points.push(a, b, c);
+}
+
+function divideTriangle(a, b, c, count)
+{
+
+    // check for end of recursion
+
+    if (count == 0) {
+        triangle(a, b, c);
+    }
+    else {
+
+        //bisect the sides
+
+        var ab = mix(a, b, 0.5);
+        var ac = mix(a, c, 0.5);
+        var bc = mix(b, c, 0.5);
+
+        --count;
+
+        // three new triangles
+
+        divideTriangle(a, ab, ac, count);
+        divideTriangle(c, ac, bc, count);
+        divideTriangle(b, bc, ab, count);
+    }
+}
 
 function transform() {
+    //theta=thetaLoc;
     var rad1 = radians(theta);
     //rad = 2;
     for (var i = 0; i < points.length; i++) {
@@ -69,36 +114,31 @@ function transform() {
         points[i] = vec2(x, y);
         var rotatedPosition = vec2(x * Math.cos(rad) - y * Math.sin(rad), x * Math.sin(rad) + y * Math.cos(rad));
         points[i] = rotatedPosition;
-    }    
-}
-
-function triangle(a, b, c) {
-    points.push(a, b, c);
-}
-
-function divideTriangle(a, b, c, count) {
-    if (count == 0) {
-        triangle(a, b, c);
-    } else {
-        var ab = mix(a, b, 0.5);
-        var bc = mix(b, c, 0.5);
-        var ca = mix(c, a, 0.5);
-        --count;
-        divideTriangle(a, ab, ca, count);
-        divideTriangle(b, bc, ab, count);
-        divideTriangle(c, ca, bc, count);
     }
 }
 
-function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.uniform1f(thetaLoc, theta);    
-    gl.drawArrays(gl.TRIANGLES, 0, points.length);    
-    setTimeout(
-            function () {
-                requestAnimFrame(render);
-            },
-            theta
-            );
+window.onload = init;
+
+function render()
+{
+//    var vertices = [
+//        vec2(-1 / 2, -1 / 2),
+//        vec2(0 / 2, 1 / 2),
+//        vec2(1 / 2, -1 / 2)
+//    ];
+    var vertices=[];
     
+    
+    points = [];
+    //theta = 90;
+    divideTriangle(vertices[0], vertices[1], vertices[2],
+            numTimesToSubdivide);
+    transform();
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
+    points = [];
+    //requestAnimFrame(render);
 }
+
+
